b450647ab27cb8846f9dcb4879338de909f576ab
CarlosAstengo <a01570405@itesm.mx>
2021-01-30 18:19:57 -0600

    Enemy now goes piu piu

---
+++ Scripts/Bullet.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+export (int) var speed
+var move = Vector2.ZERO
+var lookVec
+var player
+
+func _ready():
+	lookVec = player.position - global_position
+	
+func _physics_process(delta):
+	move = Vector2.ZERO
+	
+	move = move.move_toward(lookVec, delta)
+	move = move.normalized() * speed
+	position += move
+	
